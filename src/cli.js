922da4c85842be01e548b7dd17184d93c5285e7a
Takato Ezaki <takato.ezaki@gmail.com>
2018-05-25 07:08:30 +0000

    add list

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,38 @@
 #!/usr/bin/env node
 
-console.log("Hello, I'm Ezaki. v0.0.2")
+const inquirer = require('inquirer')
+const opn = require('opn')
+
+const cli = () => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'menu',
+      message: 'What do yo want to do?',
+      choices: [
+        'GitHub',
+        'Twitter',
+        'Exit'
+      ]
+    }
+  ]).then(answer => {
+    switch (answer.menu) {
+      case 'GitHub':
+        opn('https://github.com/ezaki', {wait: false})
+        cli()
+        break
+      case 'Twitter':
+        opn('https://twitter.com/takato_ezaki', {wait: false})
+        cli()
+        break
+      case 'Exit':
+        console.log('Bye!')
+        break
+      default:
+        console.log('Sorry. Something Wrong...')
+        cli()
+    }
+  })
+}
+
+cli()
